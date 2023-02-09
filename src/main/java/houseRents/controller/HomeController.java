124215af5ce23b41139a69e3ae0d79c9117833fa
Glenn <ithiloren@gmail.com>
2023-02-09 22:47:15 +0900

    타임리프 설정 추가

--- src/main/java/houseRents/controller/HomeController.java
+++ src/main/java/houseRents/controller/HomeController.java
@@ -1,24 +1,38 @@
 package houseRents.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = {"/",""}, method = RequestMethod.GET)
-	public String home(Model model,HttpServletRequest request) {
-		return "index.html";
+	@RequestMapping(value = {"/",""})
+	public String home(ModelAndView mnv) {
+		mnv = new ModelAndView("index");
+		System.out.println("index");
+//		mnv.setViewName("index");
+		return "index.html" ;
 	}
 	
-	@RequestMapping(value = "/current", method = RequestMethod.GET)
-	public String current(Model model,HttpServletRequest request) {
+	@RequestMapping(value = "/current")
+	public String current(ModelAndView mnv) {
+		mnv = new ModelAndView("rooms");
+		System.out.println("rooms");
+//		mnv.setViewName("rooms");
 		return "rooms.html";
 	}
-	
-	
+//	
+	@GetMapping(value = "/{roomNo}")
+	public String income(@PathVariable String roomNo, ModelAndView mnv) {
+		System.out.println(roomNo);
+		mnv = new ModelAndView("rent");
+//		mnv.setViewName("income");
+		mnv.addObject("roomNo", roomNo);
+		return "income.html";
+	}
+//	
 }
---
+++ src/main/resources/static/js/incomeInfo.js
@@ -0,0 +1,124 @@
+let infoList = {};
+
+async function getRoomInfo(){
+	let response = await fetch("/roomInfo", {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+	result = await response.json();
+	console.log(result);
+	renderRoomInfo(result.roomInfo);
+}
+function renderRoomInfo(list){
+	let roomInfoTable = document.querySelector("#roomInfoTable");
+	let js = 0, ws = 0, income=0;
+	let jb = 0, wb = 0;
+	for (let i = 0; i < list.length; i++){
+		infoList[list[i].roomNo] = list[i];
+		if(list[i].roomNo==401) continue;
+		let row = `<li onclick="openModifyModal(${list[i].roomNo})">`;
+		row += `<p class="clickable">${list[i].roomNo}</p>`;
+		row += `<p class="clickable" >`+list[i].name+'</p>';
+		row += `<p class="clickable" >`+list[i].phone+'</p>';
+		row += `<p class="clickable" >`+list[i].esName+'</p>';
+		row += `<p class="clickable" >`+list[i].esPhone+'</p>';
+		row += `<p class="clickable" >`+list[i].signDate+'</p>';
+		row += `<p class="clickable" >`+list[i].inDate+'</p>';
+		row += `<p class="clickable" >`+list[i].endDate+'</p>';
+		row += `<p class="clickable" >`+list[i].outDate+'</p>';
+		row += `<p class="clickable" >`+list[i].signType+'</p>';
+		row += `<p class="clickable" >`+list[i].deposit+'</p>';
+		row += `<p class="clickable" >`+list[i].monthly+'</p>';
+		row += '</li>';
+		roomInfoTable.insertAdjacentHTML("beforeend",row);
+		if(list[i].signType=="월세") {
+			ws++;
+			if(list[i].monthly) income+=list[i].monthly*1;
+			wb+=list[i].deposit*1;
+		}
+		if(list[i].signType=="전세") {
+			js++;
+			jb+=list[i].deposit*1;
+		}
+	}
+	
+	let row = '<li></li>';
+	roomInfoTable.insertAdjacentHTML("beforeend",row);
+	if(wb>=10000) {
+		wb=Math.round(wb/1000)/10+"억원";
+	} else {
+		wb+="만원";
+	}
+	if(jb>=10000) {
+		jb=Math.round(jb/1000)/10+"억원";
+	} else {
+		jb+="만원";
+	}
+	row = `
+			<li>
+				<p>통계</p>
+				<p>전세 : ${js}</p>
+				<p>전세보증금 : ${jb}</p>
+				<p>월세 : ${ws}</p>
+				<p>월세보증금 : ${wb}</p>
+				<p>월세 총액 : ${income}만원</p>
+			</li>
+			`;
+	roomInfoTable.insertAdjacentHTML("beforeend",row);
+}
+function openModifyModal(roomNo){
+	// let opts = document.querySelector("#form_roonNo").options;
+	// for(let i=0;i<opts.length;i++){
+	// 	if(opts[i].value==roomNo) opts[i].selected=true;
+	// }
+	console.log(roomNo);
+	document.getElementById(roomNo).selected = true;
+	form_name.value = infoList[roomNo].name;
+	form_phone.value = infoList[roomNo].phone;
+	form_esName.value = infoList[roomNo].esName;
+	form_esPhone.value = infoList[roomNo].phone;
+	form_signDate.value = infoList[roomNo].signDate;
+	form_inDate.value = infoList[roomNo].inDate;
+	form_endDate.value = infoList[roomNo].endDate;
+	form_outDate.value = infoList[roomNo].outDate;
+	console.log(infoList[roomNo].signType)
+	if(infoList[roomNo].signType!=null) document.getElementById(infoList[roomNo].signType).selected = true;
+	form_deposit.value = infoList[roomNo].deposit;
+	form_monthly.value = infoList[roomNo].monthly;
+	openModal(1);
+}
+function roomChange(){
+	openModifyModal(form_roomNo.selectedOptions[0].value);
+}
+async function insertRoomInfo(){
+	let params = {};
+	params.roomNo = form_roomNo.selectedOptions[0].value;
+	params.name = form_name.value;
+	params.phone = form_phone.value;
+	params.esName = form_esName.value;
+	params.esPhone = form_esPhone.value;
+	params.signDate = form_signDate.value;
+	params.inDate = form_inDate.value;
+	params.endDate = form_endDate.value;
+	params.outDate = form_outDate.value;
+	params.form_outDate = form_outDate.value;
+	params.signType = form_signType.selectedOptions[0].value;
+	params.deposit = form_deposit.value;
+	params.monthly = form_monthly.value;
+	console.log(params);
+	let response = await fetch("/roomInfo", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(params),
+		});
+		let result = await response.json();
+		console.log(result);
+}
+
+window.onload = function(){
+	getRoomInfo();
+}
